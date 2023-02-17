95db326dcbd246845dffbc618b517ca83a1458e3
CachorroC <juankpato87@gmail.com>
2023-02-17 14:18:53 -0500

    Signed-off-by: CachorroC <juankpato87@gmail.com>
     Please enter the commit message for your changes. Lines starting
     with '' will be ignored, and an empty message aborts the commit.
    
     On branch master
     Your branch is up to date with 'app_nuevecita/master'.
    
     Changes to be committed:
    	modified:   package.json
    	new file:   src/components/drawer.tsx
    	modified:   src/components/layout.tsx
    	modified:   src/pages/index.tsx
    	modified:   src/styles/layout.module.scss
    	modified:   yarn.lock

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,32 +1,35 @@
 // components/layout.js
-import Link from 'next/link';
-import Image from 'next/image';
-import layout from '../styles/layout.module.scss';
-import React from 'react';
+import Link from "next/link";
+import Image from "next/image";
+import layout from "../styles/layout.module.scss";
+import React from "react";
+import Drawer from "./drawer";
 
 export default function Layout(props: any) {
   return (
     <main className={layout.container}>
+      <Drawer />
       <nav className={layout.navbar}>
-        <Link href='/'>Home</Link>
-        <Link href='/offline'>About</Link>
-        <Link href='/post/first'>First Post</Link>
-        <Link href='/post/second'>Second Post</Link>
-        <Link href='/showLinks'>links</Link>
+        <Link href="/">Home</Link>
+        <Link href="/offline">About</Link>
+        <Link href="/post/first">First Post</Link>
+        <Link href="/post/second">Second Post</Link>
+        <Link href="/showLinks">links</Link>
       </nav>
       <div className={layout.main}>{props.children}</div>
       <footer className={layout.footer}>
         <Link
-          href='https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
-          target='_blank'
-          rel='noopener noreferrer'>
-          Powered by{' '}
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{" "}
           <Image
             width={240}
             height={240}
-            src='/vercel.svg'
-            alt='Vercel Logo'
-            className='logo'
+            src="/vercel.svg"
+            alt="Vercel Logo"
+            className="logo"
           />
         </Link>
       </footer>
