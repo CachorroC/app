3159a7bc795dc5de3ae58fdc307ba06b0d86ad77
CachorroC <juankpato87@gmail.com>
2023-02-18 20:58:23 -0500

    Signed-off-by: CachorroC <juankpato87@gmail.com>
     Please enter the commit message for your changes. Lines starting
     with '' will be ignored, and an empty message aborts the commit.
    
     On branch master
     Your branch is up to date with 'app_nuevecita/master'.
    
     Changes to be committed:
    	modified:   postcss.config.js
    	modified:   src/_posts/dynamic-routing.md
    	modified:   src/_posts/hello-world.md
    	modified:   src/_posts/preview.md
    	modified:   src/components/alert.tsx
    	modified:   src/components/avatar.tsx
    	modified:   src/components/cover-image.tsx
    	modified:   src/components/date-formatter.tsx
    	modified:   src/components/footer.tsx
    	modified:   src/components/header.tsx
    	modified:   src/components/hero-post.tsx
    	modified:   src/components/meta.tsx
    	modified:   src/components/more-stories.tsx
    	modified:   src/components/post-header.tsx
    	modified:   src/components/post-preview.tsx
    	modified:   src/components/post-title.tsx
    	modified:   src/components/section-separator.tsx
    	modified:   src/interfaces/author.ts
    	modified:   src/interfaces/post.ts
    	modified:   src/lib/api.ts
    	modified:   src/lib/constants.ts
    	modified:   src/lib/markdownToHtml.ts
    	modified:   src/pages/index.tsx
    	modified:   src/pages/posts/[slug].tsx
    	modified:   src/styles/css/Home.module.css
    	modified:   src/styles/css/navbar.module.css
    	modified:   src/styles/scss/Home.module.scss
    	modified:   src/styles/scss/layout.module.scss
    	modified:   src/types/remark-html.d.ts
    	modified:   tailwind.config.js

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -127,14 +127,14 @@ export default function Home({
     </>
   );
 } */
-import Container from '../components/container';
-import MoreStories from '../components/more-stories';
-import HeroPost from '../components/hero-post';
-import Intro from '../components/intro';
-import { getAllPosts } from '../lib/api';
-import Head from 'next/head';
-import { CMS_NAME } from '../lib/constants';
-import Post from '../interfaces/post';
+import Container from "../components/container";
+import MoreStories from "../components/more-stories";
+import HeroPost from "../components/hero-post";
+import Intro from "../components/intro";
+import { getAllPosts } from "../lib/api";
+import Head from "next/head";
+import { CMS_NAME } from "../lib/constants";
+import Post from "../interfaces/post";
 
 type Props = {
   allPosts: Post[];
@@ -145,39 +145,35 @@ export default function Index({ allPosts }: Props) {
   const morePosts = allPosts.slice(1);
   return (
     <>
-
-        <Head>
-          <title>{`Next.js Blog Example with ${CMS_NAME}`}</title>
-        </Head>
-        <Container>
-          <Intro />
-          {heroPost && (
-            <HeroPost
-              title={heroPost.title}
-              coverImage={heroPost.coverImage}
-              date={heroPost.date}
-              author={heroPost.author}
-              slug={heroPost.slug}
-              excerpt={heroPost.excerpt}
-            />
-          )}
-          {morePosts.length > 0 && (
-            <MoreStories posts={morePosts} />
-          )}
-        </Container>
-
+      <Head>
+        <title>{`Next.js Blog Example with ${CMS_NAME}`}</title>
+      </Head>
+      <Container>
+        <Intro />
+        {heroPost && (
+          <HeroPost
+            title={heroPost.title}
+            coverImage={heroPost.coverImage}
+            date={heroPost.date}
+            author={heroPost.author}
+            slug={heroPost.slug}
+            excerpt={heroPost.excerpt}
+          />
+        )}
+        {morePosts.length > 0 && <MoreStories posts={morePosts} />}
+      </Container>
     </>
   );
 }
 
 export const getStaticProps = async () => {
   const allPosts = getAllPosts([
-    'title',
-    'date',
-    'slug',
-    'author',
-    'coverImage',
-    'excerpt',
+    "title",
+    "date",
+    "slug",
+    "author",
+    "coverImage",
+    "excerpt",
   ]);
 
   return {
