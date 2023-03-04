c3841221a1964cddf63a91af21ebdf540f50fcb1
CachorroC <juankpato87@gmail.com>
2023-03-04 17:20:44 -0500

    Signed-off-by: CachorroC <juankpato87@gmail.com>
     Por favor ingresa el mensaje del commit para tus cambios. Las
      líneas que comiencen con '' serán ignoradas, y un mensaje
      vacío aborta el commit.
    
     En la rama master
     Tu rama está adelantada a 'origin/master' por 4 commits.
       (usa "git push" para publicar tus commits locales)
    
     Cambios a ser confirmados:
    	modificados:     .vscode/settings.json
    	nuevos archivos: src/components/appbar.tsx
    	modificados:     src/components/header.tsx

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,13 +1,14 @@
-import Navbar from "../components/navbar";
-import Drawer from "../components/drawer";
-import styles from "../styles/css/layout.module.css"
+import Navbar from '../components/navbar';
+import Drawer from '../components/drawer';
+import styles from '../styles/css/layout.module.css';
+import DrawerAppBar from './appbar';
 
-export default function Header ()
-{
+export default function Header() {
   return (
     <div className={styles.header}>
-      <Navbar/>
+      <Navbar />
+      <DrawerAppBar />
       <Drawer />
     </div>
-  )
+  );
 }
