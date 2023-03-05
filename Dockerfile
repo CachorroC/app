bffca4a0eb28a6ee66751ea6a3261055d2f84974
CachorroC <juankpato87@gmail.com>
2023-03-05 12:46:43 -0500

    Signed-off-by: CachorroC <juankpato87@gmail.com>
     Please enter the commit message for your changes. Lines starting
     with '' will be ignored, and an empty message aborts the commit.
    
     On branch master
     Your branch is up to date with 'origin/master'.
    
     Changes to be committed:
    	modified:   .vscode/settings.json
    	modified:   Dockerfile
    	modified:   package.json
    	deleted:    public/fallback-a6cmTIuOm_PaHLD_z-f2R.js
    	new file:   public/fallback-development.js
    	modified:   public/sw.js
    	new file:   public/sw.js.map
    	new file:   public/workbox-327c579b.js
    	new file:   public/workbox-327c579b.js.map
    	deleted:    public/workbox-588899ac.js
    	modified:   src/components/header.tsx

--- Dockerfile
+++ Dockerfile
@@ -60,4 +60,3 @@ EXPOSE 3000
 ENV PORT 3000
 
 CMD npm start
-
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,14 +1,115 @@
 import Navbar from '../components/navbar';
-import Drawer from '../components/drawer';
 import styles from '../styles/css/layout.module.css';
 import DrawerAppBar from './appbar';
+import 'material-symbols';
+import * as React from 'react';
+import {
+  AppBar,
+  Box,
+  Button,
+  Divider,
+  Drawer,
+  IconButton,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+  Toolbar,
+  Typography,
+} from '@mui/material';
+import Link from 'next/link';
+
+const drawerWidth = 240;
+const navItems = [
+  '/',
+  'planets',
+  'blog',
+  'showLinks',
+  'post',
+  'posts',
+  'posts/preview',
+  'api/getLinks',
+];
 
 export default function Header() {
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen((prevState) => !prevState);
+  };
+
+  const drawer = (
+    <Box
+      onClick={handleDrawerToggle}
+      sx={{ textAlign: 'center' }}
+    >
+      <Typography
+        variant='h6'
+        sx={{ my: 2 }}
+      >
+        MUI
+      </Typography>
+      <Divider />
+      <List>
+        {navItems.map((item) => (
+          <Link
+            key={item}
+            href={item}
+          >
+            <ListItemButton sx={{ textAlign: 'center' }}>
+              <ListItemText primary={item} />
+            </ListItemButton>
+          </Link>
+        ))}
+      </List>
+    </Box>
+  );
   return (
-    <div className={styles.header}>
-      <Navbar />
-      <DrawerAppBar />
-      <Drawer />
-    </div>
+    <>
+      <>
+        <AppBar component='nav'>
+          <Toolbar className={styles.header}>
+            <IconButton
+              aria-label='open drawer'
+              edge='start'
+              onClick={handleDrawerToggle}
+            >
+              <span className='material-symbols-outlined'>
+                density_medium
+              </span>
+            </IconButton>
+            <Box>
+              {navItems.map((item) => (
+                <Button
+                  key={item}
+                  sx={{ color: '#fff' }}
+                >
+                  {item}
+                </Button>
+              ))}
+            </Box>
+          </Toolbar>
+        </AppBar>
+        <Box component='nav'>
+          <Drawer
+            variant='temporary'
+            open={mobileOpen}
+            onClose={handleDrawerToggle}
+            ModalProps={{ keepMounted: true }}
+            sx={{
+              display: { xs: 'block', sm: 'none' },
+              '& .MuiDrawer-paper': {
+                boxSizing: 'border-box',
+                width: drawerWidth,
+              },
+            }}
+          >
+            {drawer}
+          </Drawer>
+        </Box>
+      </>
+      <Toolbar />
+      <Toolbar />
+    </>
   );
 }
