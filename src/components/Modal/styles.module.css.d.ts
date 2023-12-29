b8334173a429514c66fdb876449a2e2b89ffee05
CachorroC <juankpato87@gmail.com>
2023-12-29 11:17:16 -0500

    Signed-off-by: CachorroC <juankpato87@gmail.com>
    2 Please enter the commit message for your changes. Lines starting
    2 with '2' will be ignored, and an empty message aborts the commit.
    2
    2 On branch urgent-unicorn-fixer
    2 Your branch is up to date with 'origin/urgent-unicorn-fixer'.
    2
    2 Changes to be committed:
    2	modified:   src/app/Carpeta/[numero]/styles.module.css.d.ts
    2	modified:   src/app/Carpetas/@right/Nueva/styles.module.css.d.ts
    2	deleted:    src/app/apple-icon-precomposed.png
    2	deleted:    src/app/apple-icon.png
    2	deleted:    src/app/apple-icon.svg
    2	deleted:    src/app/apple-icon1-precomposed.png
    2	modified:   src/app/apple-icon1.png
    2	deleted:    src/app/apple-icon2-precomposed.png
    2	modified:   src/app/apple-icon2.png
    2	deleted:    src/app/apple-icon3-precomposed.png
    2	modified:   src/app/apple-icon3.png
    2	deleted:    src/app/apple-icon4-precomposed.png
    2	modified:   src/app/apple-icon4.png
    2	deleted:    src/app/apple-icon5-precomposed.png
    2	modified:   src/app/apple-icon5.png
    2	new file:   src/app/apple-icon6.png
    2	new file:   src/app/favicon.ico
    2	deleted:    src/app/icon.png
    2	modified:   src/app/icon.svg
    2	modified:   src/app/icon1.png
    2	deleted:    src/app/icon10.png
    2	deleted:    src/app/icon11.png
    2	deleted:    src/app/icon12.png
    2	deleted:    src/app/icon13.png
    2	deleted:    src/app/icon14.png
    2	deleted:    src/app/icon15.png
    2	modified:   src/app/icon2.png
    2	modified:   src/app/icon3.png
    2	modified:   src/app/icon4.png
    2	modified:   src/app/icon5.png
    2	modified:   src/app/icon6.png
    2	modified:   src/app/icon7.png
    2	deleted:    src/app/icon8.png
    2	deleted:    src/app/icon9.png
    2	deleted:    src/app/logo.svg
    2	deleted:    src/app/next.svg
    2	deleted:    src/app/safari-pinned-tab.svg
    2	modified:   src/components/Accordion/accordion.module.css.d.ts
    2	modified:   src/components/Buttons/buttons.module.css.d.ts
    2	modified:   src/components/Calendar/calendar.module.css.d.ts
    2	modified:   src/components/Card/card.module.css.d.ts
    2	modified:   src/components/Modal/styles.module.css.d.ts
    2	modified:   src/components/Nota/note.module.css.d.ts
    2	modified:   src/components/Proceso/procesos.module.css.d.ts
    2	modified:   src/components/form/checkbox/styles.module.css.d.ts
    2	modified:   src/components/form/form.module.css.d.ts
    2	modified:   src/components/layout/navbar.module.css.d.ts
    2	modified:   src/components/layout/search/searchbar.module.css.d.ts
    2	modified:   src/styles/box.module.css.d.ts
    2	modified:   src/styles/fonts/typeface.module.css.d.ts
    2	modified:   src/styles/fonts/typography.module.css.d.ts
    2	modified:   src/styles/globals.css.d.ts
    2	modified:   src/styles/layout.module.css.d.ts
    2	modified:   src/styles/theme/colors.module.css.d.ts
    2	modified:   src/styles/theme/variables.css.d.ts
    2	modified:   src/styles/theme/variables.module.css.d.ts
    2	modified:   src/styles/variables.module.css.d.ts
    2

--- src/components/Modal/styles.module.css.d.ts
+++ src/components/Modal/styles.module.css.d.ts
@@ -1,11 +1,11 @@
 declare const styles: {
-  readonly 'closed': string;
-  readonly 'fadein': string;
-  readonly 'fadeout': string;
-  readonly 'open': string;
-  readonly 'show': string;
-  readonly 'snackbar': string;
-  readonly 'wrapper': string;
+  readonly "closed": string;
+  readonly "fadein": string;
+  readonly "fadeout": string;
+  readonly "open": string;
+  readonly "show": string;
+  readonly "snackbar": string;
+  readonly "wrapper": string;
 };
 export = styles;
 
