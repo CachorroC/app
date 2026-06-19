bf7eb82e21417da953991442dc4541c28e186342
CachorroC <juankpato87@gmail.com>
2026-06-18 19:05:12 -0500

    Signed-off-by: CachorroC <juankpato87@gmail.com>
     Please enter the commit message for your changes. Lines starting
     with '' will be ignored, and an empty message aborts the commit.
    
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   src/lib/project/utils/main.ts

--- src/lib/project/utils/main.ts
+++ src/lib/project/utils/main.ts
@@ -5,7 +5,7 @@ export const capitalize = ( str: string ) => {
   return str?.replace(
     /\b\w/g, ( substr ) => {
       return substr.toUpperCase();
-    } 
+    }
   );
 };
 
@@ -18,31 +18,31 @@ export const consultaProcesosPorRazonSocial = ( nombre: string ) => {
 
 export const consultaProcesoDetalleURL = ( idProceso: string ) => {
   return new URL(
-    `Proceso/Detalle/${ idProceso }`, ramaJudicialBaseURL 
+    `Proceso/Detalle/${ idProceso }`, ramaJudicialBaseURL
   );
 };
 
 export const consultaProcesoDocumentosURL = ( idProceso: string ) => {
   return new URL(
-    `Proceso/Detalle/${ idProceso }`, ramaJudicialBaseURL 
+    `Proceso/Detalle/${ idProceso }`, ramaJudicialBaseURL
   );
 };
 
 export const ramaJudicialProcesosPorNumeroRad = ( llaveProceso: string ) => {
   return new URL(
-    `Procesos/Consulta/NumeroRadicacion?numero=${ llaveProceso }&SoloActivos=false&pagina=1`,
+    `Procesos/Consulta/NumeroRadicacion?numero=${ llaveProceso }&SoloActivos=false`,
     ramaJudicialBaseURL,
   );
 };
 
 export const ramaJudicialDescargadeDocsProceso = ( idProceso: string ) => {
   return new URL(
-    `Descarga/DOCX/Proceso/${ idProceso }`, ramaJudicialBaseURL 
+    `Descarga/DOCX/Proceso/${ idProceso }`, ramaJudicialBaseURL
   );
 };
 
 export const ramaJudicialDescargadeCSVProceso = ( idProceso: string ) => {
   return new URL(
-    `Descarga/CSV/Detalle/${ idProceso }`, ramaJudicialBaseURL 
+    `Descarga/CSV/Detalle/${ idProceso }`, ramaJudicialBaseURL
   );
 };
